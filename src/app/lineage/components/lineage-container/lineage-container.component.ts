9fc5249b72b3fcacee455355bffb52c3cd4a357e
Assaf Shemesh <assaf.sun@gmail.com>
2020-12-05 17:44:03 +0200

    Support in 3d graph and align containers css

--- src/app/lineage/components/lineage-container/lineage-container.component.ts
+++ src/app/lineage/components/lineage-container/lineage-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import ForceGraph3D from '3d-force-graph';
 
 @Component({
   selector: 'lineage-container',
@@ -8,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class LineageContainerComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit(): void {
+        // Random tree
+        const N = 300;
+        const gData = {
+          nodes: [...Array(N).keys()].map(i => ({ id: i })),
+          links: [...Array(N).keys()]
+            .filter(id => id)
+            .map(id => ({
+              source: id,
+              target: Math.round(Math.random() * (id-1))
+            }))
+        };
+    
+        const Graph = ForceGraph3D()
+          (document.getElementById('3d-graph'))
+            .graphData(gData);
   }
-
 }
